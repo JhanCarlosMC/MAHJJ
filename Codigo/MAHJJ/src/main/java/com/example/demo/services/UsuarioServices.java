567f3a6a225325341645ed1bb602da4d2d403b02
mari8292 <marieth8292@gmail.com>
2022-09-13 17:02:37 -0500

    Update UsuarioServices.java

--- Codigo/MAHJJ/src/main/java/com/example/demo/services/UsuarioServices.java
+++ Codigo/MAHJJ/src/main/java/com/example/demo/services/UsuarioServices.java
@@ -34,6 +34,86 @@ public class UsuarioServices {
     }
 
 
+    // Metodos Marieth Perpiñan 
+    
+    /**
+    *  El sistema permite consultar todos los usuarios
+    *
+    * 
+    * @return List<Usuario>
+    */
+    public List<Usuario> getAllUsers(){
+        List<Usuario> users = null;
+        try {
+            users = new ArrayList<>();
+            users.addAll(usuarioRepository.findAll());
+        } catch (Exception e) {
+            System.err.println("ERROR");
+        }
+        return  users;
+    }
+    
+    /**
+    * El sistema permite consultar un solo usuario
+    *
+    * @param id 
+    * @return Usuario
+    */
+    public Usuario getUser(Long id){
+        Usuario user = null;
+        try {
+            user = usuarioRepository.getReferenceById(id);
+        } catch (Exception e) {
+            System.err.println("ERROR");
+        }
+        return user;
+    }
+
+    /**
+    * El sistema permite crear un usuario
+    *
+    * @param usuario 
+    * @return Usuario
+    */
+    public Usuario createUser(Usuario usuario){
+        Usuario user = null;
+        try {
+            user = usuarioRepository.save(usuario);
+        } catch (Exception e) {
+        }
+        return user;
+    }
+    
+    /**
+    * El sistema permite editar un usuario
+    *
+    * @param usuario 
+    * @return Usuario
+    */
+    public Usuario editUser(Usuario usuario){
+        Usuario user = null;
+        try {
+            user = usuarioRepository.saveAndFlush(usuario);
+        } catch (Exception e) {
+        }
+        return user;
+    }
+
+    /**
+    * El sistema permite eliminar un usuario
+    *
+    * @param id 
+    * @return Usuario
+    */
+    public boolean deleteUser(Long id){
+        Boolean delete = false;
+        try {
+            usuarioRepository.deleteById(id);
+            delete = true;
+        } catch (Exception e) {
+        }
+        return delete;
+    }
 
 
 }
