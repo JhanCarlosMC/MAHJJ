c4978bc3711587815f721361582c9a3e4167bc25
harold <hehurtado9@misena.edu.co>
2022-09-13 16:48:38 -0500

    CERACION DE MODELO ENTRESPRISE  Y DEL REPOSITORIO

--- Codigo/MAHJJ/src/main/java/com/example/demo/model/Enterprise.java
+++ Codigo/MAHJJ/src/main/java/com/example/demo/model/Enterprise.java
@@ -1,9 +1,35 @@
 package com.example.demo.model;
 
-import javax.persistence.OneToMany;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 
 public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column( name = "nombre",unique = true)
+    private String  nombre;
 
-    @OneToMany
-    private Usuario usuario;
+
+    @Column( name = "documento",unique = true)
+    private String  documento;
+
+
+
+    private  String telefono;
+    private  String  direccion;
+
+    private Date updateAT;
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
