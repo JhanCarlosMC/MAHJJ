8016cc0fdfea59c13103bffe36aa7fc1a543d489
harold <hehurtado9@misena.edu.co>
2022-09-13 18:43:47 -0500

    Revert "CERACION DE MODELO ENTRESPRISE  Y DEL REPOSITORIO"
    This reverts commit c4978bc3711587815f721361582c9a3e4167bc25.

--- Codigo/MAHJJ/src/main/java/com/example/demo/model/Enterprise.java
+++ Codigo/MAHJJ/src/main/java/com/example/demo/model/Enterprise.java
@@ -1,35 +1,9 @@
 package com.example.demo.model;
 
-import javax.persistence.*;
-import java.util.Date;
-
-@Entity
+import javax.persistence.OneToMany;
 
 public class Enterprise {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
-    private Long id;
-    @Column( name = "nombre",unique = true)
-    private String  nombre;
 
-
-    @Column( name = "documento",unique = true)
-    private String  documento;
-
-
-
-    private  String telefono;
-    private  String  direccion;
-
-    private Date updateAT;
-    private Date createdAt;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @OneToMany
+    private Usuario usuario;
 }
